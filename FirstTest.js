81d171afa89d01b5cb0616bb3221b64da2e01bbb
bettercallsully <solomonelkelani@gmail.com>
2021-02-14 12:41:56 -0600

    Microverse Project #1, and Code Challenge #1

--- FirstTest.js
+++ FirstTest.js
@@ -2,14 +2,13 @@
 function beautifulDays(i, j, k) {
     let diasHermosos = [20,21,22,23]
     for(i=0;i = diasHermosos.length;i++){
-        let revers = diasHermosos.toString().split('').revers().join('')
+        let reverse = diasHermosos.toString().split('').reverse().join('')
 
     }
 // 20 = 2
 // 21= 12
 // 22= 22
 // 23= 32
-console.log(revers)
+console.log(reverse)
 console.log("Hi")
-
 }
