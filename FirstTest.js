f91a0cb0803cd2f2d4a370004c60361fe2ae2481
VictorOmarT.M <33044106+VictorOmarT@users.noreply.github.com>
2021-02-14 11:36:10 -0600

    1rs

---
+++ FirstTest.js
@@ -0,0 +1,15 @@
+
+function beautifulDays(i, j, k) {
+    let diasHermosos = [20,21,22,23]
+    for(i=0;i = diasHermosos.length;i++){
+        let revers = diasHermosos.toString().split('').revers().join('')
+
+    }
+// 20 = 2
+// 21= 12
+// 22= 22
+// 23= 32
+console.log(revers)
+console.log("Hi")
+
+}
