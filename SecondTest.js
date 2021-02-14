6752cff5a661d18e5658ca84d817e3a83a031a2a
bettercallsully <solomonelkelani@gmail.com>
2021-02-14 15:53:48 -0600

    SullyCodingChallenge2Complete

---
+++ SecondTest.js
@@ -0,0 +1,17 @@
+let danWithoutPotion = 4;
+let track = [1, 6, 3, 5, 2]
+function hurdleRace(k, height) {
+    height = 4;
+    k = track 
+    counter = 0;
+    for (i = 0; i = k; i++) {
+        if (height <= k[i]) {
+            return 0; 
+
+    } else {
+        return counter +1;
+        }
+    }  
+    return counter;
+}
+console.log(hurdleRace(4,[1,6,3,5,2]))
